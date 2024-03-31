355f728cbb906575f41d92b8dc94b672866c4677
Vladimir Zelenokor <119796161+VladimirZelenokor1@users.noreply.github.com>
2024-03-31 15:40:48 +0300

    Update Ex1_done.sql

--- CodeBasics - SQL/Ex1/Ex1_done.sql
+++ CodeBasics - SQL/Ex1/Ex1_done.sql
@@ -1,8 +1,5 @@
 USE moviesdb;
-SELECT * FROM movies ORDER BY release_year DESC;
-SELECT * FROM movies WHERE release_year=2022;
-SELECT * FROM movies WHERE release_year > 2020;
-SELECT * FROM movies WHERE release_year > 2020 AND imdb_rating>8;
-SELECT * FROM movies WHERE studio IN ("Marvel studios", "Hombale Films");
-SELECT * FROM movies WHERE title LIKE "%THOR%" ORDER BY release_year;
-SELECT * FROM movies WHERE studio NOT IN ("Marvel studios");
+SELECT title, release_year from movies where studio="Marvel Studios"
+SELECT * from movies where title LIKE '%Avenger%'
+SELECT release_year from movies where title="The Godfather"
+SELECT DISTINCT studio from movies where industry="Bollywood"
